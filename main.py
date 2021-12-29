8fc74a9450b1870f6e970d83844c94ea93d21347
drapkinamria <drapkinamaria02@gmail.com>
2021-12-29 14:35:02 +0500

    one more commit

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import nested_dict as nd
 
-f = open('ekaterinburg.txt', 'r')
+f = open('ekaterinburg.txt', 'r', encoding="utf-8")
 new_dictionary = nd.nested_dict()
+count = 0
+
+
 for line in f:
     if "id" in line and "lat" in line and "lon" in line:
+        count += 1
         k = int(line.find("id"))
         d = int(line.find("lat"))
         t = int(line.find("lon"))
-        print(line)
         id = line[k + 4: k + 13]
         lat = line[d + 5: d + 15]
         lan = line[t + 5: t + 15]
-        print(id)
-        print(lat)
-        print(lan)
-        #new_dictionary[lat][lan] = id
+        new_dictionary[lat][lan] = id
+print(new_dictionary)
