187403f29f30e0deefcf12a3592869ae4717e147
Aleksey <a.e.sokolkov@gmail.com>
2021-12-31 20:44:10 +0500

    working ways

--- main.py
+++ main.py
@@ -16,24 +16,51 @@ def get_nodes(lines):
     return new_dictionary
 
 
-def get_way(row, text):
+def get_by_beginning(needle, text):
+    index = text.find(needle)
+    if index == -1:
+        raise ValueError
+    start = index + len(needle)
+    end = text[start:].find("\"")
+    return text[start:start + end]
+
+
+def find_nodes(lines):
+    result = []
+    for i in lines:
+        if "nd ref=\"" in i:
+            result.append(get_by_beginning("nd ref=\"", i))
+    return result
+
+
+def find_tags(lines):
+    result = {}
+    for i in lines:
+        if "<tag k=\"" in i:
+            result[get_by_beginning("<tag k=\"", i)] = get_by_beginning("v=\"", i)
+    return result
+
+
+def get_ways(text):
+    result = {}
     local_text = text[text.find("<way "):text.rfind("</way>")]
     for way in local_text.split("</way>"):
-        all_exists = False
-        for i in row.split():
-            if i not in way[way.find("<tag"):way.rfind("</tag")]:
-                all_exists = True
-                break
-        if all_exists:
-            return way
+        try:
+            i = get_by_beginning("id=\"", way)
+        except ValueError:
+            continue
+        lines = way.split("\n")[1:]
+        nodes = find_nodes(lines)
+        tags = find_tags(lines)
+        result[i] = {'nodes': nodes, 'tags': tags}
+    return result
 
 
 def main():
     row = "Екатеринбург Ленина 51"
     text = read_file("ekaterinburg.txt")
     nodes = get_nodes(text.split("\n"))
-    way = get_way(row, text)
-    x = 1
+    ways = get_ways(text)
 
 
 if __name__ == '__main__':
