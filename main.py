3da091355d23e1e597f1c964a1067c289f4806ca
Aleksey <a.e.sokolkov@gmail.com>
2022-01-03 00:46:18 +0500

    alpha1

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
 from files import read_file
-import nested_dict as nd
-
-
-def get_nodes(lines):
-    new_dictionary = nd.nested_dict()
-    for line in lines:
-        if "id" in line and "lat" in line and "lon" in line:
-            k = int(line.find("id"))
-            d = int(line.find("lat"))
-            t = int(line.find("lon"))
-            id = line[k + 4: k + 13]
-            lat = line[d + 5: d + 14]
-            lan = line[t + 5: t + 15]
-            new_dictionary[lat][lan] = id
-    return new_dictionary
 
 
 def get_by_beginning(needle, text):
@@ -23,6 +8,19 @@ def get_by_beginning(needle, text):
     start = index + len(needle)
     end = text[start:].find("\"")
     return text[start:start + end]
+
+
+def get_nodes(text):
+    result = {}
+    for line in text.split("\n"):
+        try:
+            i = get_by_beginning("id=\"", line)
+            lat = get_by_beginning("lat=\"", line)
+            lon = get_by_beginning("lon=\"", line)
+            result[i] = {"lat": lat, "lon": lon}
+        except ValueError:
+            continue
+    return result
 
 
 def find_nodes(lines):
@@ -37,7 +35,13 @@ def find_tags(lines):
     result = {}
     for i in lines:
         if "<tag k=\"" in i:
-            result[get_by_beginning("<tag k=\"", i)] = get_by_beginning("v=\"", i)
+            value = get_by_beginning("v=\"", i)
+            if "улица" in value or "Улица" in value or \
+                    "street" in value or "Street" in value:
+                value = value.replace("улица", "").replace("Улица", "")
+                value = value.replace("street", "").replace("Street", "")
+                value = value.replace(" ", "")
+            result[get_by_beginning("<tag k=\"", i)] = value
     return result
 
 
@@ -56,11 +60,65 @@ def get_ways(text):
     return result
 
 
+def find_variants_tier1(words, ways):
+    result = []
+    for way in ways.values():
+        for word in words:
+            if "tags" in way and word in way["tags"].values():
+                result.append(way)
+    return result
+
+
+def find_variants_tier2(words, variants):
+    result = []
+    for variant in variants:
+        all_here = True
+        for word in words:
+            if word not in variant["tags"].values():
+                all_here = False
+        if all_here:
+            result.append(variant)
+    return result
+
+
+def find_nodes_ids(variants, nodes):
+    result = []
+    for variant in variants:
+        for node in variant["nodes"]:
+            if node in nodes:
+                result.append(node)
+    return result
+
+
+def find_coords(variants, nodes):
+    result = []
+    for i in find_nodes_ids(variants, nodes):
+        result.append(nodes[i])
+    return result
+
+
+def print_coords(coords):
+    t = ["N", "LAT", "LON"]
+    print("=======================================")
+    print("Подходящие варианты:")
+    print(f'{t[0]:5} {t[1]:15} {t[2]:10}')
+    for i in range(len(coords)):
+        print(f'{i:5} {coords[i]["lat"]:15} {coords[i]["lon"]:15}')
+    print("=======================================")
+
+
 def main():
-    row = "Екатеринбург Ленина 51"
+    print("Введите данные через пробел")
+    print("Пример: Екатеринбург Восточная")
+    words = input().split()
     text = read_file("ekaterinburg.txt")
-    nodes = get_nodes(text.split("\n"))
+    nodes = get_nodes(text)
     ways = get_ways(text)
+    vars_tier1 = find_variants_tier1(words, ways)
+    vars_tier2 = find_variants_tier2(words, vars_tier1)
+    final_vars = vars_tier2[:5] if vars_tier2 else vars_tier1[:5]
+    coords = find_coords(final_vars, nodes)
+    print_coords(coords)
 
 
 if __name__ == '__main__':
