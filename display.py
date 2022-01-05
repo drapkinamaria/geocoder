ae2cf3dc5c00dfe18a99324facfc674410581259
Aleksey <a.e.sokolkov@gmail.com>
2022-01-06 02:10:04 +0500

    log /r

--- display.py
+++ display.py
@@ -4,5 +4,5 @@ def print_coords(coords):
     print("Подходящие варианты:")
     print(f'{t[0]:5} {t[1]:15} {t[2]:10}')
     for i in range(len(coords)):
-        print(f'{i:5} {coords[i]["lat"]:15} {coords[i]["lon"]:15}')
+        print(f'{i+1:5} {coords[i]["lat"]:15} {coords[i]["lon"]:15}')
     print("=======================================")
--- main.py
+++ main.py
@@ -7,20 +7,20 @@ from files import read_file, download_district
 
 def main():
     district = get_district_filename(input("Введите федеральный округ: "))
-    if not exists(f".\\districts\\{district}"):
-        print("Округ не найден. Идет скачивание.")
-        download_district(district)
-        print("Округ скачан.")
-    print("Чтение файла.")
-    data = read_file(f".\\districts\\{district}")
-    print("Файл прочитан.")
-    nodes = get_nodes(data)
-    print("Узлы получены.")
-    ways = get_ways(data)
-    print("Пути получены.")
-
     words = input("Введите место: ").lower().split()
     simplify_words(words)
+
+    if not exists(f".\\districts\\{district}"):
+        print("\rОкруг не найден. Идет скачивание.", end="")
+        download_district(district)
+        print("\rОкруг скачан.", end="")
+    print("\rЧтение файла.   [1/4]", end="")
+    data = read_file(f".\\districts\\{district}")
+    print("\rФайл прочитан.  [2/4]", end="")
+    nodes = get_nodes(data)
+    print("\rУзлы получены.  [3/4]", end="")
+    ways = get_ways(data)
+    print("\rПути получены.  [4/4]", end="")
 
     variant = find_best_way(words, ways)
     if variant == ("", 0):
