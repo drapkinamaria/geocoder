d7536494e71d146addca57ad07f0cbcce5a57d59
Aleksey <a.e.sokolkov@gmail.com>
2022-01-06 01:27:16 +0500

    download_district()

--- files.py
+++ files.py
@@ -1,8 +1,20 @@
+import time
+import urllib.request
+import bz2
+
+
 def read_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return file.read()
 
 
 def download_district(district):
-    # TODO
-    pass
+    url = f"http://download.geofabrik.de/russia/{district}.bz2"
+    try:
+        file = urllib.request.urlretrieve(url, f"./districts/{district}.bz2")
+    except Exception:
+        raise ValueError("Ссылка скачивания больше недействительна.")
+    time.sleep(3)
+    z = bz2.BZ2File(f"./districts/{district}.bz2")
+    open(f"./districts/{district}", 'wb').write(z.read())
+    time.sleep(3)
